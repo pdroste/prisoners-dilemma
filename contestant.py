b7725fcc9a61f408de28109a7421daa5c41aa139
pdroste <droste.paul@gmx.de>
2023-12-26 21:47:08 +0100

    Add contestant.py
    Add file for the Contestant class which will handle the move selection and tracking based on a specific strategy to the prisoner's dilemma problem.

---
+++ contestant.py
@@ -0,0 +1,9 @@
+#!interpreter [optional-arg]
+# -*- coding: utf-8 -*-
+
+"""
+Contestant class handling the move selection and tracking based on a specific
+strategy to the prisoner's dilemma problem.
+
+License: MIT License
+"""
