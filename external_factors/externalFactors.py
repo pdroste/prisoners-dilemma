ffaf95b93ac53092c82c3aa9e744ba613a2ee39b
pdroste <droste.paul@gmx.de>
2023-12-27 12:25:49 +0100

    Implement ExternalFactors
    Implement ExternalFactors class that provides a method of adding external influences on the prisoner's dilemma problem, such as imperfect communication.

--- external_factors/externalFactors.py
+++ external_factors/externalFactors.py
@@ -7,3 +7,44 @@ prisoner's dilemma problem.
 
 License: MIT License
 """
+
+from abc import ABC, abstractmethod
+
+class ExternalFactors(ABC):
+
+    @abstractmethod
+    def __init__(self):
+        """
+        Constructor for the `ExternalFactors` class.
+        """
+        pass
+
+    @abstractmethod
+    def change_perception(self, move, *args):
+        """
+        Method for changing the perception of a contestant's move in the eyes of
+        the second contestant.
+
+        :param move:        move to change perception of
+        :type move:         bool
+        :param args:        additional arguments to be specified by
+                            implementation of method
+        :return:            new perception of given move
+        :rtype:             bool
+        """
+        pass
+
+    @abstractmethod
+    def change_result(self):
+        """
+        Method for changing the result of a contestant's move in the eyes of
+        both contestants.
+
+        :param move:        move to change result of
+        :type move:         bool
+        :param args:        additional arguments to be specified by
+                            implementation of method
+        :return:            new result of given move
+        :rtype:             bool
+        """
+        pass
