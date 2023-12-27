776fd6704ca28da425bdccecb0ef14addfa9d98e
pdroste <droste.paul@gmx.de>
2023-12-27 12:15:45 +0100

    Add externalFactors.py
    Add module for implementation of abstract class to base external influences on prisoner's dilemma problem on.

---
+++ external_factors/externalFactors.py
@@ -0,0 +1,9 @@
+#!interpreter [optional-arg]
+# -*- coding: utf-8 -*-
+
+"""
+Abstract strategy class template for external factors influencing the
+prisoner's dilemma problem.
+
+License: MIT License
+"""
